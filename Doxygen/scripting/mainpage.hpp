1b0340ab61b65a7693f1602e833da9b0967aa1d5
Carsten Fuchs <carsten.fuchs@cafu.de>
2011-03-15 09:11:38 +0000

    Scripting reference documentation: Augmented main page / overview text.
    git-svn-id: https://srv7.svn-repos.de/dev123/projects/cafu/trunk@275 5fe0b3f3-48df-4727-b460-e6dda9226989

--- Doxygen/scripting/mainpage.hpp
+++ Doxygen/scripting/mainpage.hpp
@@ -5,25 +5,42 @@
  *
  * Welcome to the <em>Cafu Engine Scripting Documentation</em>.
  *
+ * You can use scripts to program the behaviour of things in the Cafu universe.
+ *
  * This reference documentation describes the classes and functions that can occur and be used in Cafu game scripts, especially:
  *
- *   - Map scripts &ndash; Scripts for game maps that can control the entities that live within the map.
- *   - GUI scripts &ndash; Scripts that control the user interaction with the 2D GUIs (e.g. the programs main menu)
- *     and the 3D GUIs (e.g. the teleporter controls, unlock door keypads, etc.)
+ *   - Map scripts &ndash; Scripts that program and control the entities that live within a game world
+ *     (such as human players, monsters, weapons, doors, lifts, ...).
+ *   - GUI scripts &ndash; Scripts that control the user interaction with 2D GUIs (e.g. the programs main menu)
+ *     and 3D GUIs (e.g. the teleporter controls, unlock door keypads, etc.).
  *
  * Other sub-systems in Cafu are script-controlled as well, such as the in-game console,
  * the program configuration file, the render materials and sound shaders, etc.
  * For these types of script, please refer to the documentation at http://www.cafu.de/wiki/
  *
  *
+ * @section gettingstarted How do I get started?
+ *
+ * For getting started with scripting, the "learning-by-doing" approach often works very well, because it's fast and fun:
+ * Start by studying and modifing the example scripts that ship with Cafu (see links below),
+ * or take them as starting points for your own work.
+ * Experimenting with working scripts will make it easy for you to understand the underlying key ideas and concepts.
+ * Then come back here and read the documentation as required.
+ * You may also want to have a look at the Lua documentation and/or start asking questions at the Cafu mailing list or forum.
+ *
+ *
  * @section moreinfo Additional Resources of Information
  *
- *   - For an overview and introduction to Cafu scripting, see the documentation at http://www.cafu.de/wiki/
+ *   - For an overview and introduction to Cafu development, see the documentation at http://www.cafu.de/wiki/
  *   - The Developer Resources page is at http://www.cafu.de/developers
- *   - The website of the Lua programming language is http://www.lua.org
+ *   - The scripting implementation in Cafu is based on the programming language <a href="http://www.lua.org">Lua</a>.
+ *       - Learn more about Lua at http://www.lua.org/about.html
+ *       - Check out the very good Lua documentation at http://www.lua.org/docs.html <br />
+ *         It provides all the background information about the language and is an indispensable resource for advanced Cafu scripting!
  *   - For example scripts, see
- *        - http://trac.cafu.de/browser/cafu/trunk/Games/DeathMatch/Worlds for map scripts, and
- *        - http://trac.cafu.de/browser/cafu/trunk/Games/DeathMatch/GUIs for GUI scripts.
+ *       - http://trac.cafu.de/browser/cafu/trunk/Games/DeathMatch/Worlds for map scripts, and
+ *       - http://trac.cafu.de/browser/cafu/trunk/Games/DeathMatch/GUIs for GUI scripts.
+ *       - These scripts also ship with each Cafu release and are found in the respective subdirectories.
  *   - If you're looking for the \emph{C++} reference documentation instead, see http://api.cafu.de
  */
 
