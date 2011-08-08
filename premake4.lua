1a77fd1ddf891314dc68b375d459c496daf4c118
Carsten Fuchs <carsten.fuchs@cafu.de>
2011-08-08 19:50:09 +0000

    Updated/continued our experimental `premake4.lua` script.
    git-svn-id: https://srv7.svn-repos.de/dev123/projects/cafu/trunk@346 5fe0b3f3-48df-4727-b460-e6dda9226989

--- premake4.lua
+++ premake4.lua
@@ -4,10 +4,14 @@ function prepend(s, t)
 end
 
 
+-- Global variable _ACTION can be nil, for instance if you type `premake4 --help`.
+BUILD_PATH = "build/" .. (_ACTION or "")
+
+
 -- A solution contains projects, and defines the available configurations
 solution "CafuEngine"
     configurations { "Debug", "Release" }
-    location "build"    -- Generate solution files in "build" subdirectory.
+    location(BUILD_PATH)   -- Generate solution files in this subdirectory.
 
     -- Global settings.
     -- defines { "..." }
@@ -18,6 +22,7 @@ solution "CafuEngine"
         -- -- /EHsc  Enable exception handling.
         -- buildoptions { "/GR", "/EHsc" }      -- TODO: Does premake4 set this by default?
         defines { "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_DEPRECATE" }
+        includedirs { "ExtLibs/zlib" }
         flags { "StaticRuntime", "No64BitChecks" }
 
     configuration "Debug"
@@ -25,21 +30,79 @@ solution "CafuEngine"
         -- envDebug.Append(LINKFLAGS=["/debug"]);
         flags { "Symbols" }
         defines { "DEBUG" }
+        targetdir(BUILD_PATH .. "/bin/Debug")
 
     configuration "Release"
         -- envRelease.Append(CCFLAGS=Split("/MT /O2 /Ob2"));
         flags { "OptimizeSpeed", "NoIncrementalLink" }
         defines { "NDEBUG" }    -- Need NDEBUG to have the assert macro generate no runtime code.
+        targetdir(BUILD_PATH .. "/bin/Release")
 
 
 project "lua"
     kind "StaticLib"
     language "C"
+    -- I've taken the list of source files directly from the lua/INSTALL file.
     files(prepend("ExtLibs/lua/src/", string.explode([[lapi.c lcode.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c
             lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c
             ltable.c ltm.c lundump.c lvm.c lzio.c
             lauxlib.c lbaselib.c ldblib.c liolib.c lmathlib.c loslib.c
             ltablib.c lstrlib.c loadlib.c linit.c]], "%s+")))
+
+
+project "bulletmath"
+    kind "StaticLib"
+    language "C++"
+    includedirs { "ExtLibs/bullet/src" }
+    files "ExtLibs/bullet/src/LinearMath/**.cpp"
+
+project "bulletcollision"
+    kind "StaticLib"
+    language "C++"
+    includedirs { "ExtLibs/bullet/src" }
+    files "ExtLibs/bullet/src/BulletCollision/**.cpp"
+
+project "bulletdynamics"
+    kind "StaticLib"
+    language "C++"
+    includedirs { "ExtLibs/bullet/src" }
+    files "ExtLibs/bullet/src/BulletDynamics/**.cpp"
+
+project "bulletsoftbody"
+    kind "StaticLib"
+    language "C++"
+    includedirs { "ExtLibs/bullet/src" }
+    files "ExtLibs/bullet/src/BulletSoftBody/**.cpp"
+
+
+project "lightwave"
+    kind "StaticLib"
+    language "C"
+    includedirs { "ExtLibs/lwo" }
+    files "ExtLibs/lwo/*.c"
+    excludes "ExtLibs/lwo/main.c"
+
+
+project "minizip"
+    kind "StaticLib"
+    language "C"
+    files { "ExtLibs/minizip/unzip.c", "ExtLibs/minizip/ioapi.c" }
+
+
+project "png"
+    kind "StaticLib"
+    language "C"
+    files "ExtLibs/libpng/*.c"
+    excludes { "ExtLibs/libpng/example.c", "ExtLibs/libpng/pngtest.c" }
+
+
+project "z"
+    kind "StaticLib"
+    language "C"
+    files "ExtLibs/zlib/*.c"
+    excludes { "ExtLibs/zlib/example.c", "ExtLibs/zlib/minigzip.c" }
+
+
 
 
 --[[
@@ -57,3 +120,16 @@ project "CaBSP"
     -- links
     -- libdirs
 --]]
+
+
+if _ACTION == "clean" then
+    for a in premake.action.each() do   -- action trigger is "vs2008", "gmake", etc.
+        dir = "build/" .. a.trigger
+        if os.isdir(dir) then
+            local result, msg=os.rmdir(dir)
+            -- os.rmdir() always seems to return nil...
+            -- print("Removing directory " .. dir .. "... " .. (result and 'ok!' or msg))
+            print("Removing directory " .. dir .. "...")
+        end
+    end
+end
