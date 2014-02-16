d598445dddff9ba24a1662ecee91bffd6443be00
Carsten Fuchs <carsten.fuchs@cafu.de>
2014-02-16 11:13:03 +0100

    DeathMatch: Remove the obsolete and now unused `EntFuncDoorT` class.

--- Games/DeathMatch/Code/GameImpl.cpp
+++ Games/DeathMatch/Code/GameImpl.cpp
@@ -37,7 +37,6 @@ For support and more information about Cafu, visit us at <http://www.cafu.de>.
 
 #include "CompanyBot.hpp"
 #include "Corpse.hpp"
-#include "FuncDoor.hpp"
 #include "HumanPlayer.hpp"
 #include "InfoGeneric.hpp"
 #include "InfoPlayerStart.hpp"
@@ -98,7 +97,6 @@ namespace GAME_NAME
     const cf::TypeSys::TypeInfoT* AllTypeInfos[] = {
         &EntCompanyBotT::TypeInfo,
         &EntCorpseT::TypeInfo,
-        &EntFuncDoorT::TypeInfo,
         &EntHumanPlayerT::TypeInfo,
         &EntInfoGenericT::TypeInfo,
         &EntInfoPlayerStartT::TypeInfo,
