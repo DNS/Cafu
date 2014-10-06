d8f32897ff86b312eb879942ad7dd73e1612ebb9
Carsten Fuchs <carsten.fuchs@cafu.de>
2014-10-06 10:13:41 +0200

    Map Editor: Rewrite the material-related commands to no longer use the obsolete `MapElementT::Assign()` method for their "undo" operation.
    The two previous commits "almost" allowed us to get rid of the principally
    problematic `MapElementT::Assign()` method that also turned out to be very
    inefficient for implementing the "undo" operation in our commands (because
    the related "old state" clones of the objects contained a huge amount of
    unneeded state).
    
    This commit changes the material-related commands to no longer use the obsolete
    `MapElementT::Assign()` method for their "undo" operation. As the new
    implementation only saves the state that is actually needed in order to perform
    the "undo" operation, as a side effect the efficiency is much improved.

--- CaWE/MapEditor/Commands/ApplyMaterial.cpp
+++ CaWE/MapEditor/Commands/ApplyMaterial.cpp
@@ -29,20 +29,53 @@ For support and more information about Cafu, visit us at <http://www.cafu.de>.
 
 CommandApplyMaterialT::CommandApplyMaterialT(MapDocumentT& MapDoc, const ArrayT<MapElementT*>& Objects, EditorMaterialI* Material)
     : m_MapDoc(MapDoc),
-      m_Objects(Objects),
       m_Material(Material)
 {
-    for (unsigned long ObjNr=0; ObjNr<m_Objects.Size(); ObjNr++)
-        m_OldStates.PushBack(m_Objects[ObjNr]->Clone());
-}
+    for (unsigned int i = 0; i < Objects.Size(); i++)
+    {
+        MapBrushT*       Brush       = dynamic_cast<MapBrushT*>(Objects[i]);
+        MapBezierPatchT* BezierPatch = dynamic_cast<MapBezierPatchT*>(Objects[i]);
+        MapTerrainT*     Terrain     = dynamic_cast<MapTerrainT*>(Objects[i]);
 
+        if (Brush)
+        {
+            m_Brushes.PushBack(Brush);
+            m_Objects.PushBack(Brush);
+        }
+        else if (BezierPatch)
+        {
+            m_BezierPatches.PushBack(BezierPatch);
+            m_Objects.PushBack(BezierPatch);
+        }
+        else if (Terrain)
+        {
+            m_Terrains.PushBack(Terrain);
+            m_Objects.PushBack(Terrain);
+        }
+    }
 
-CommandApplyMaterialT::~CommandApplyMaterialT()
-{
-    for (unsigned long i=0; i<m_OldStates.Size(); i++)
-        delete m_OldStates[i];
+    m_OldBrushMats.PushBackEmptyExact(m_Brushes.Size());
 
-    m_OldStates.Clear();
+    for (unsigned int i = 0; i < m_Brushes.Size(); i++)
+    {
+        MapBrushT*        Brush = m_Brushes[i];
+        ArrayT<MapFaceT>& Faces = Brush->GetFaces();
+
+        m_OldBrushMats[i].PushBackEmptyExact(Faces.Size());
+
+        for (unsigned int FaceNr = 0; FaceNr < Faces.Size(); FaceNr++)
+            m_OldBrushMats[i][FaceNr] = Faces[FaceNr].GetMaterial();
+    }
+
+    m_OldBezierPatchMats.PushBackEmptyExact(m_BezierPatches.Size());
+
+    for (unsigned int i = 0; i < m_BezierPatches.Size(); i++)
+        m_OldBezierPatchMats[i] = m_BezierPatches[i]->GetMaterial();
+
+    m_OldTerrainMats.PushBackEmptyExact(m_Terrains.Size());
+
+    for (unsigned int i = 0; i < m_Terrains.Size(); i++)
+        m_OldTerrainMats[i] = m_Terrains[i]->GetMaterial();
 }
 
 
@@ -51,31 +84,20 @@ bool CommandApplyMaterialT::Do()
     wxASSERT(!m_Done);
     if (m_Done) return false;
 
-    for (unsigned long ObjNr=0; ObjNr<m_Objects.Size(); ObjNr++)
+    for (unsigned int i = 0; i < m_Brushes.Size(); i++)
     {
-        MapElementT* Obj=m_Objects[ObjNr];
+        MapBrushT*        Brush = m_Brushes[i];
+        ArrayT<MapFaceT>& Faces = Brush->GetFaces();
 
-        if (Obj->GetType()==&MapBrushT::TypeInfo)
-        {
-            MapBrushT*        Brush=(MapBrushT*)Obj;
-            ArrayT<MapFaceT>& Faces=Brush->GetFaces();
-
-            for (unsigned long FaceNr=0; FaceNr<Faces.Size(); FaceNr++)
-                Faces[FaceNr].SetMaterial(m_Material);
-        }
-        else if (Obj->GetType()==&MapBezierPatchT::TypeInfo)
-        {
-            MapBezierPatchT* BP=(MapBezierPatchT*)Obj;
-
-            BP->SetMaterial(m_Material);
-        }
-        else if (Obj->GetType()==&MapTerrainT::TypeInfo)
-        {
-            MapTerrainT* Terrain=(MapTerrainT*)Obj;
-
-            Terrain->SetMaterial(m_Material);
-        }
+        for (unsigned int FaceNr = 0; FaceNr < Faces.Size(); FaceNr++)
+            Faces[FaceNr].SetMaterial(m_Material);
     }
+
+    for (unsigned int i = 0; i < m_BezierPatches.Size(); i++)
+        m_BezierPatches[i]->SetMaterial(m_Material);
+
+    for (unsigned int i = 0; i < m_Terrains.Size(); i++)
+        m_Terrains[i]->SetMaterial(m_Material);
 
     m_MapDoc.UpdateAllObservers_Modified(m_Objects, MEMD_SURFACE_INFO_CHANGED);
 
@@ -87,11 +109,22 @@ bool CommandApplyMaterialT::Do()
 void CommandApplyMaterialT::Undo()
 {
     wxASSERT(m_Done);
-
     if (!m_Done) return;
 
-    for (unsigned long ObjNr=0; ObjNr<m_Objects.Size(); ObjNr++)
-        m_Objects[ObjNr]->Assign(m_OldStates[ObjNr]);
+    for (unsigned int i = 0; i < m_Brushes.Size(); i++)
+    {
+        MapBrushT*        Brush = m_Brushes[i];
+        ArrayT<MapFaceT>& Faces = Brush->GetFaces();
+
+        for (unsigned int FaceNr = 0; FaceNr < Faces.Size(); FaceNr++)
+            Faces[FaceNr].SetMaterial(m_OldBrushMats[i][FaceNr]);
+    }
+
+    for (unsigned int i = 0; i < m_BezierPatches.Size(); i++)
+        m_BezierPatches[i]->SetMaterial(m_OldBezierPatchMats[i]);
+
+    for (unsigned int i = 0; i < m_Terrains.Size(); i++)
+        m_Terrains[i]->SetMaterial(m_OldTerrainMats[i]);
 
     m_MapDoc.UpdateAllObservers_Modified(m_Objects, MEMD_SURFACE_INFO_CHANGED);
 
