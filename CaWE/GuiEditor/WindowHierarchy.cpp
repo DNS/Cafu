07b40867f24a4862e78d87aa1cd2f5588450ca70
Carsten Fuchs <carsten.fuchs@cafu.de>
2015-08-15 13:48:17 +0200

    GUI Editor: Make sure that selected windows are visible when the Window Hierarchy dialog is refreshed.
    This change does for the GUI Editor what the previous change did for the
    Map Editor.

--- CaWE/GuiEditor/WindowHierarchy.cpp
+++ CaWE/GuiEditor/WindowHierarchy.cpp
@@ -301,6 +301,15 @@ void WindowHierarchyT::RefreshTree()
             Expand(Result);
     }
 
+    // Make sure that the selected elements are visible.
+    for (unsigned long SelNr = 0; SelNr < Selection.Size(); SelNr++)
+    {
+        wxTreeItemId Result = FindTreeItem(ID, Selection[SelNr]);
+
+        if (Result.IsOk())
+            EnsureVisible(Result);
+    }
+
     Thaw();
 }
 
