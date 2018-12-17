8bafd6111d80b04f64e1220bc5c23b560fca7fe5
Carsten Fuchs <carsten.fuchs@cafu.de>
2018-12-17 10:49:06 +0100

    Fix paths to images

--- docs/dw_filter.lua
+++ docs/dw_filter.lua
@@ -8,6 +8,7 @@ end
 
 -- This function strips wrapper <div> elements that are not needed
 -- and cause redundant `container::` blocks in reStructuredText output.
+-- For example, <li><div class="li">...</div></li> becomes <li>...</li>.
 function Div(div)
     if div.attr.identifier ~= "" then return end
     if #div.attr.attributes ~= 0 then return end
@@ -51,6 +52,10 @@ function Link(link)
      -- print("tag: " .. link.tag .. "  " .. link.t)
     end
 
+    -- Turns <a href="..."> HTML hyperlinks into internal references in RST, where appropriate.
+    -- For example, <a href="/modeleditor:mainwindow" class="wikilink1" title="modeleditor:mainwindow">The Main Window</a>
+    -- becomes :ref:`the_main_window` in RST.
+    -- See https://groups.google.com/d/msg/pandoc-discuss/Nqv_6TyQZa0/_7lf0O-PCAAJ for details.
     if #link.attr.classes > 0 and link.attr.classes[1] == "wikilink1" then
         -- print("~~~~~~~~~~~~~~")
         tg = ""
@@ -70,5 +75,47 @@ function Link(link)
         return pandoc.RawInline("rst", ":ref:`" .. tg .. "`")
     end
 
+    -- Replace <a href="/_detail/..."><img .../></a> with <img .../>.
+    -- DokuWiki produced low-res editions of the images and linked to their full-res counterparts.
+    -- As we always use the full-res editions, we don't need the wrapper link.
+    if #link.attr.classes > 0 and link.attr.classes[1] == "media" and link.target:sub(1, 9) == "/_detail/" then
+        return link.content
+    end
+
     return link
+end
+
+
+-- Fixes the paths to our images.
+function Image(img)
+    if false then
+        print("")
+        print("neues Bild:")
+
+        io.write("caption: ")
+        for k,v in pairs(img.caption) do
+            if v.tag == "Str" then
+                io.write("'" .. v.text .. "' ")
+            else
+                io.write(v.tag .. " ")
+            end
+        end
+        io.write("\n")
+
+        print("src: " .. img.src)
+        print("title: " .. img.title)
+        print("id: " .. img.attr.identifier)
+        printTable("classes: ", img.attr.classes)
+        printTable("attribs: ", img.attr.attributes)
+     -- print("tag: " .. img.tag .. "  " .. img.t)
+    end
+
+    if img.src:find("/_media/") then
+        img.src = img.src:gsub("/_media/", "/images/")
+        img.src = img.src:gsub(":", "/")
+        img.src = img.src:gsub("%?.*", "")
+        -- print(" ----> " .. img.src)
+    end
+
+    return img
 end
